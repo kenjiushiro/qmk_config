4912e866a1400b97a0468843716622312ace790f
kenjiushiro <kenjiushiro@kenjiushiro.penji.com>
2023-09-24 11:59:25 -0400

    Move functions and enum into their own files

---
+++ enums.c
@@ -0,0 +1,43 @@
+enum custom_keycodes
+{
+    GMAIL_EMAIL = SAFE_RANGE,
+    LIVE_EMAIL,
+    WORK_EMAIL,
+    WORK_EMAIL2,
+    DNI,
+    PHONE_NUMBER,
+    ALT_TAB,
+    LSALT_TAB,
+    DESKTOP_LEFT,
+    DESKTOP_RIGHT,
+    DESKTOP_UP,
+    SWITCH_TO_MAC,
+    SWITCH_TO_WINDOWS,
+    SWITCH_TO_LINUX,
+    WORD_MODIFIER,
+    UNDO,
+    CUT,
+    COPY,
+    PASTE,
+    SCREENSHOT,
+    ENIE,
+    MISSION_CONTROL,
+};
+
+enum OS
+{
+    _MAC,
+    _WINDOWS,
+    _LINUX,
+};
+
+int currentOS = _WINDOWS;
+enum layers
+{
+    _QWERTY,
+    _NUMERIC,
+    _NAVIGATION,
+    _SYMBOLS,
+    _FN_KEYS,
+    _CONFIG
+};
