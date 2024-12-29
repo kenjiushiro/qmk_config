5920745d2dc9fb8646a670a74b43cf359867a241
Kenji <kenji.m.ushiro@gsk.com>
2024-12-29 17:19:57 -0300

    add gaming layer

--- utils.c
+++ utils.c
@@ -3,6 +3,8 @@
 #define SYM_LYR MO(_SYMBOLS)
 #define FN_LYR MO(_FN_KEYS)
 #define CONF_LYR MO(_CONFIG)
+#define GAMING_LYR DF(_GAMING)
+#define DEFAULT_LYR DF(_QWERTY)
 
 bool     is_alt_tab_active = false; // ADD this near the begining of keymap.c
 uint16_t alt_tab_timer     = 0;
